74c118d998710204b0ad8b862fd10c24409b2ea2
Erica Nahintsara Kone <nerica@c1r6s16.42antananarivo.mg>
2024-06-23 16:22:00 +0300

    start liste chainee

--- push_swap.c
+++ push_swap.c
@@ -1,18 +1,53 @@
 
 
 #include "push_swap.h"
+#include <stdio.h>
+
+int	check_double(int argc, char *argv[])
+{
+	int i;
+	int j;
+
+	i = 1;
+	j = i + 1;
+	while (i < argc)
+	{
+		while (j < argc)
+		{
+			if (ft_atoi(argv[i]) == ft_atoi(argv[j]))
+				return(1);
+			j++;
+		}
+		i++;
+		j = i + 1;
+	}
+	return(0);
+}
+
+Item	*initialize()
+{
+	Item	*stack;
+
+	stack = malloc (sizeof(List));
+	if (stack == NULL)
+		exit(EXIT_FAILURE);
+	stack->next = NULL;
+	stack->previous = NULL;
+	return (stack);
+}
 
 int main(int argc, char *argv[])
 {
-	if (argc == 1)
-		return (0);
-	int i;
-	i = 1;
-	while (i < argc - 1)
+	Item	a_stack;
+	Item	b_stack;
+
+	if (argc == 1 ||check_double(argc, argv) == 1)
 	{
-		if(ft_atoi(argv[i]) != 0)
-			ft_printf("hello");
-		i++;
+		ft_printf("Error");
+		return(0);
 	}
+	a_stack = initialize();
+	b_stack = initialize();
+	
 	return 0;
 }
--- push_swap.h
+++ push_swap.h
@@ -2,5 +2,17 @@
 # define PUSH_SWAP_H
 
 # include "libftprintf/libft.h"
-
+# include <stdlib.h>
+typedef struct Item Item;
+struct Item
+{
+	int	value;
+	Item	*next;
+	Item	*previous;
+};
+typedef struct List List;
+struct List
+{
+	Item	*first;	
+};
 #endif /*__PUSH_SWAP_H__*/
